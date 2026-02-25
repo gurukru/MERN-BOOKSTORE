e8c51545c784d4bca74b78db923b9edc9b9be840
gurukru <chm2026.gurukrurai@gmail.com>
2026-02-25 08:55:20 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 const api = axios.create({
-    baseURL: 'https://mern-bookstore-0fna.onrender.com/'
+    baseURL: 'https://mern-bookstore-0fna.onrender.com'
 })
 export default api
