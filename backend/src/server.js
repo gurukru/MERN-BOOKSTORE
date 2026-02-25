fa401a639a565d05978e6c549ad00389272ecf75
gurukru <chm2026.gurukrurai@gmail.com>
2026-02-25 08:45:48 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,14 +7,14 @@ import cors from "cors";
 dotenv.config()
 const app=express();
 const port=process.env.PORT || 3001
-app.use(cors({
-    origin:"http://localhost:5173"
-}))
+// app.use(cors({
+//     origin:"http://localhost:5173"
+// }))
 app.use(express.json())
 app.use("/books", bookRoutes)
 connectDB().then(()=>{
 app.listen(port, () =>{
-    // console.log(`http://localhost:${port}/books`)
+    console.log(`http://localhost:${port}/books`)
 })
 })
 // app.get('/',(req,res)=>{
